4685b036beb3e66280a904120c9f866822290f2c
shyamcodes <veerchauhan0211@gmail.com>
2024-12-12 17:17:28 +0530

    2D Array print spiral matrix and sum of diagonal matrix.

---
+++ 04_2D_Arrays/_01matrices.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class _01matrices {
+
+  public static boolean search(int matrix[][], int key) {
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        if (matrix[i][j] == key) {
+          System.out.print("Found at cell (" + i + "," + j + ")");
+          return true;
+        }
+      }
+    }
+    System.out.println("Key not found");
+    return false;
+  }
+
+  public static void main(String args[]) {
+    int matrix[][] = new int[3][3];
+    int n = matrix.length, m = matrix[0].length;
+
+    // Taking Input
+    Scanner sc = new Scanner(System.in);
+
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        matrix[i][j] = sc.nextInt();
+      }
+    }
+
+    // Printing Output
+    for (int i = 0; i < n; i++) {
+      for (int j = 0; j < m; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+    search(matrix, 6);
+
+  }
+}
